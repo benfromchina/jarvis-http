70a1ef8f83b276947b4855ae2dd8e158c27d474e
Ben <mengbin@hotmail.com>
2025-10-20 14:38:20 +0800

    bugfix: 解决 forest 丢失查询参数导致签名校验失败

--- jarvis-http-client/src/main/java/com/stark/jarvis/http/client/http/forest/ServiceFactory.java
+++ jarvis-http-client/src/main/java/com/stark/jarvis/http/client/http/forest/ServiceFactory.java
@@ -1,24 +1,15 @@
 package com.stark.jarvis.http.client.http.forest;
 
 import com.dtflys.forest.Forest;
-import com.dtflys.forest.backend.HttpBackend;
-import com.dtflys.forest.backend.httpclient.HttpclientBackend;
-import com.dtflys.forest.backend.okhttp3.OkHttp3Backend;
 import com.dtflys.forest.config.ForestConfiguration;
 import com.dtflys.forest.converter.json.ForestJacksonConverter;
 import com.dtflys.forest.converter.json.ForestJsonConverter;
 import com.dtflys.forest.interceptor.Interceptor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.stark.jarvis.cipher.core.AsymmetricAlgorithm;
 import com.stark.jarvis.http.client.config.Config;
-import com.stark.jarvis.http.client.config.RSAConfig;
-import com.stark.jarvis.http.client.config.SM2Config;
-import com.stark.jarvis.http.client.constant.SystemConsts;
+import com.stark.jarvis.http.client.util.ForestUtils;
 import com.stark.jarvis.http.client.util.JacksonUtils;
 import lombok.Builder;
-import okhttp3.OkHttpClient;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.client.HttpClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -68,23 +59,7 @@ public class ServiceFactory {
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false));
 
-        if (AsymmetricAlgorithm.SM2.equals(SystemConsts.CLIENT_ASYMMETRIC_ALGORITHM)) {
-            clientConfig = new SM2Config.Builder()
-                    .clientId(SystemConsts.CLIENT_ID)
-                    .clientSecret(SystemConsts.CLIENT_SECRET)
-                    .clientCert(SystemConsts.CLIENT_CERT)
-                    .clientPrivateKey(SystemConsts.CLIENT_PRIVATE_KEY)
-                    .serverPublicKey(SystemConsts.SERVER_PUBLIC_KEY)
-                    .build();
-        } else {
-            clientConfig = new RSAConfig.Builder()
-                    .clientId(SystemConsts.CLIENT_ID)
-                    .clientSecret(SystemConsts.CLIENT_SECRET)
-                    .clientCert(SystemConsts.CLIENT_CERT)
-                    .clientPrivateKey(SystemConsts.CLIENT_PRIVATE_KEY)
-                    .serverPublicKey(SystemConsts.SERVER_PUBLIC_KEY)
-                    .build();
-        }
+        clientConfig = ForestUtils.getClientConfig();
     }
 
     /**
@@ -111,37 +86,12 @@ public class ServiceFactory {
         config.setJsonConverter(forestJsonConverter);
 
         List<Class<? extends Interceptor>> interceptors = new ArrayList<>();
+        interceptors.add(SetVariablesInterceptor.class);
         interceptors.add(SignRequestInterceptor.class);
         interceptors.add(ValidateResponseInterceptor.class);
         config.setInterceptors(interceptors);
 
-        config.setVariable("config", clientConfig);
-        config.setVariable("userAgent", getUserAgent(clazz, config));
         return config.client(clazz);
-    }
-
-    private String getUserAgent(Class<?> clazz, ForestConfiguration config) {
-        return String.format(
-                SystemConsts.USER_AGENT_FORMAT,
-                StringUtils.defaultIfBlank(clazz.getPackage().getImplementationVersion(), "Unknown"),
-                SystemConsts.OS,
-                StringUtils.defaultIfBlank(SystemConsts.JAVA_VERSION, "Unknown"),
-                clientConfig.getClientCredential().getClass().getSimpleName(),
-                clientConfig.getServerResponseValidator().getClass().getSimpleName(),
-                getHttpClientInfo(config.getBackend()));
-    }
-
-    protected String getHttpClientInfo(HttpBackend getBackend) {
-        String clientName = getBackend.getName();
-        String clientVersion;
-        if (HttpclientBackend.NAME.equals(clientName)) {
-            clientVersion = HttpClient.class.getPackage().getImplementationVersion();
-        } else if (OkHttp3Backend.NAME.equals(clientName)) {
-            clientVersion = OkHttpClient.class.getPackage().getImplementationVersion();
-        } else {
-            clientVersion = "Unknown";
-        }
-        return clientName + "/" + clientVersion;
     }
 
 }
