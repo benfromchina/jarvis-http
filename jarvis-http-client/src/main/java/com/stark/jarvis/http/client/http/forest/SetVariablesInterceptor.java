8b85a03980691d5a6668fff3dc0052c72def330d
Ben <mengbin@hotmail.com>
2025-10-30 09:17:00 +0800

    移除测试代码及控制台输出

--- jarvis-http-client/src/main/java/com/stark/jarvis/http/client/http/forest/SetVariablesInterceptor.java
+++ jarvis-http-client/src/main/java/com/stark/jarvis/http/client/http/forest/SetVariablesInterceptor.java
@@ -1,15 +1,10 @@
 package com.stark.jarvis.http.client.http.forest;
 
-import com.dtflys.forest.backend.url.QueryableURLBuilder;
-import com.dtflys.forest.backend.url.URLBuilder;
 import com.dtflys.forest.config.ForestConfiguration;
 import com.dtflys.forest.http.ForestRequest;
 import com.dtflys.forest.interceptor.ForestInterceptor;
 import com.stark.jarvis.http.client.config.Config;
 import com.stark.jarvis.http.client.util.ForestUtils;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 /**
  * 设置参数拦截器
@@ -20,22 +15,8 @@ import java.net.URISyntaxException;
  */
 public class SetVariablesInterceptor implements ForestInterceptor {
 
-    private static final URLBuilder URL_BUILDER = new QueryableURLBuilder();
-
     @Override
     public boolean beforeExecute(ForestRequest request) {
-        String url = URL_BUILDER.buildUrl(request);
-        try {
-            URI uri = new URI(url);
-            String canonicalUrl = uri.getRawPath();
-            if (uri.getQuery() != null) {
-                canonicalUrl += "?" + uri.getRawQuery();
-            }
-            System.out.println("-->canonicalUrl=" + canonicalUrl);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
         ForestConfiguration config = request.getConfiguration();
         Config clientConfig = ForestUtils.getClientConfig();
         config.setVariable("config", clientConfig);
